f8a8d407da956dd3dfe254bb74c8782775fe33b1
SaiD-MH <muhamdsaid253@gmail.com>
2024-02-21 10:09:50 +0200

    Adding project dependencies

---
+++ src/main/java/backend/jobportal/OnlineJobPortalApplication.java
@@ -0,0 +1,13 @@
+package backend.jobportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineJobPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineJobPortalApplication.class, args);
+	}
+
+}
